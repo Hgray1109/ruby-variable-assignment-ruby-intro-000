5ce88c34dc057d5b8eb0f2bddc32559003d66c68
Hashem Gray <hgray1109@gmail.com>
2021-05-06 01:36:39 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -4,4 +4,5 @@ describe "./variable.rb" do
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
     greeting = "hello World" "./variable.rb" "greeting" end
 
+  end
   
